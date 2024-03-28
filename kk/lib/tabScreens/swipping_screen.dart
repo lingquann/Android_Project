cfce211bf6c120234559837e8aeca4b5e2aeb565
mingkwon <minhquan6303@gmail.com>
2024-03-29 02:00:25 +0700

    swipping

--- kk/lib/tabScreens/swipping_screen.dart
+++ kk/lib/tabScreens/swipping_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kk/controllers/profile-controller.dart';
+import 'package:get/get.dart';
 
 
 class SwippingScreen extends StatefulWidget 
@@ -11,19 +14,33 @@ class SwippingScreen extends StatefulWidget
 
 class _SwippingScreenState extends State<SwippingScreen> 
 {
+  ProfileController profileController = Get.put(ProfileController());
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Swipping Screen",
-          style: TextStyle(
-            color: Colors.red,
-            fontSize: 20,
-            
-          ),
-        ),
-      ),
+    return Scaffold(
+      body: Obx(()
+      {
+        return PageView.builder(
+        itemCount: profileController.allUsersProfileList.length,
+        controller: PageController(initialPage: 0, viewportFraction: 1),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index)
+        {
+          final eachProfileInfo = profileController.allUsersProfileList[index];
+
+          return DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  eachProfileInfo.imageProfile.toString(),
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      );
+      })
     );
   }
 }
