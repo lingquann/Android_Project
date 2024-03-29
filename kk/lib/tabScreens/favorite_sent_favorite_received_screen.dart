f4db9e5ce2658224c6d999c47b04c09a3d94033c
mingkwon <minhquan6303@gmail.com>
2024-03-29 15:40:11 +0700

    favorites

--- kk/lib/tabScreens/favorite_sent_favorite_received_screen.dart
+++ kk/lib/tabScreens/favorite_sent_favorite_received_screen.dart
@@ -1,27 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kk/global.dart';
 
-
-class FavoriteSentFavoriteReceivedScreen extends StatefulWidget 
-{
+class FavoriteSentFavoriteReceivedScreen extends StatefulWidget {
   const FavoriteSentFavoriteReceivedScreen({super.key});
 
   @override
-  State<FavoriteSentFavoriteReceivedScreen> createState() => _FavoriteSentFavoriteReceivedScreenState();
+  State<FavoriteSentFavoriteReceivedScreen> createState() =>
+      _FavoriteSentFavoriteReceivedScreenState();
 }
 
-class _FavoriteSentFavoriteReceivedScreenState extends State<FavoriteSentFavoriteReceivedScreen> 
-{
+class _FavoriteSentFavoriteReceivedScreenState
+    extends State<FavoriteSentFavoriteReceivedScreen> {
+  bool isFavoriteSentClicked = true;
+  List<String> favoriteSentList = [];
+  List<String> favoriteReceivedList = [];
+  List favoritesList = [];
+
+  getFavoriteListKeys() async {
+    if (isFavoriteSentClicked) {
+      var favoriteSentDocument = await FirebaseFirestore.instance
+          .collection("user")
+          .doc(currentUserID.toString())
+          .collection("favoriteSent")
+          .get();
+      for (int i = 0; i < favoriteSentDocument.docs.length; i++) {
+        favoriteSentList.add(favoriteSentDocument.docs[i].id);
+      }
+
+      getKeysDataFromUsersCollection(favoriteSentList);
+    } 
+    else {
+      var favoriteReceivedDocument = await FirebaseFirestore.instance
+          .collection("user")
+          .doc(currentUserID.toString())
+          .collection("favoriteReceived")
+          .get();
+      for (int i = 0; i < favoriteReceivedDocument.docs.length; i++) {
+        favoriteReceivedList.add(favoriteReceivedDocument.docs[i].id);
+      }
+
+      getKeysDataFromUsersCollection(favoriteReceivedList);
+
+    }
+  }
+
+  getKeysDataFromUsersCollection(List<String> keysList) async
+  {
+    var allUsersDocument = await FirebaseFirestore.instance.collection("user").get();
+
+    for(int i=0; i<allUsersDocument.docs.length; i++)
+    {
+      for(int k=0; k<keysList.length; k++)
+      {
+        if(((allUsersDocument.docs[i].data() as dynamic)["uid"]) == keysList[k])
+        {
+          favoritesList.add(allUsersDocument.docs[i].data());
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
       body: Center(
         child: Text(
           "Favorite Sent Favorite Received Screen",
           style: TextStyle(
             color: Colors.red,
             fontSize: 20,
-            
           ),
         ),
       ),
